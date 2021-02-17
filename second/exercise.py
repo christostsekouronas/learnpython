147f67c43840947cf9ed4e05da4a7d2b09ae5d8e
Christos Tsekouronas <christos.tsekouronas@leafnet.com.cy>
2021-02-17 12:39:56 +0200

    exercise

---
+++ second/exercise.py
@@ -0,0 +1,8 @@
+quote = """ Alright, but apart from the Sanitation, the Medicine, Education, Wine, Public Order, Irrigation, Roads, the Frensh-Water System, and Public Health, what have the Romans ever done for us? """
+
+# Use a for loop and and if statement to print just the capitals in the quote above.
+
+for char in quote:
+    if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        print(char)
+
