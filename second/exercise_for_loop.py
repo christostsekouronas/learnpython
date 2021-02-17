956147fdbef6f3151060a3964fa75897758c61f0
Christos Tsekouronas <christos.tsekouronas@leafnet.com.cy>
2021-02-17 16:17:35 +0200

    new

--- second/exercise_for_loop.py
+++ second/exercise_for_loop.py
@@ -1,4 +1,6 @@
-quote = """ Alright, but apart from the Sanitation, the Medicine, Education, Wine, Public Order, Irrigation, Roads, the Frensh-Water System, and Public Health, what have the Romans ever done for us? """
+quote = """ Alright, but apart from the Sanitation, 
+the Medicine, Education, Wine, Public Order, Irrigation, Roads, the Frensh-Water System, 
+and Public Health, what have the Romans ever done for us? """
 
 # Use a for loop and and if statement to print just the capitals in the quote above.
 
