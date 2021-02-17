2c3d0665782ad963330548bd280c058626afae31
Christos Tsekouronas <christos.tsekouronas@leafnet.com.cy>
2021-02-17 14:47:41 +0200

    new

--- second/ranges.py
+++ second/ranges.py
@@ -1,2 +1,10 @@
 for i in range(1, 20):
     print("i is now {}".format(i))
+
+# begins from 0 goes to 10 with a 2 step
+for i in range(0, 10, 2):
+    print("i is now {}".format(i))
+
+# begins from 10 goes to 0 with a -2 step
+for i in range(10, 0, -2):
+    print("i is now {}".format(i))
