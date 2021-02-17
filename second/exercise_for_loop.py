f98a6113ea3752548236afba1936d784e5f8369d
Christos Tsekouronas <christos.tsekouronas@leafnet.com.cy>
2021-02-17 16:19:44 +0200

    new

--- second/exercise_for_loop.py
+++ second/exercise_for_loop.py
@@ -1,3 +1,5 @@
+# we put """ at the start and end of a sentence if we want to change line
+
 quote = """ Alright, but apart from the Sanitation, 
 the Medicine, Education, Wine, Public Order, Irrigation, Roads, the Frensh-Water System, 
 and Public Health, what have the Romans ever done for us? """
