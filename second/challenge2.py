c9f5d880b9633346550d1b77c69a4718d96eacf5
Christos Tsekouronas <christos.tsekouronas@leafnet.com.cy>
2021-02-26 09:27:49 +0200

    new code

--- second/challenge2.py
+++ second/challenge2.py
@@ -1,6 +1,6 @@
 import random
 
-highest = 10
+highest = 1000
 answer = random.randint(1, highest)
 print(answer)    # TODO: Remove after testing
 guess = 0 # initialise to any number that doesn't equal the answer
@@ -9,11 +9,14 @@ print("Please guess number between 1 and {}: ".format(highest))
 while guess != answer:
     guess = int(input())
 
+    if guess == 0:
+        break
     if guess == answer:
             print("Well done, you guessed it")
+            break
     else:
         if guess < answer:
-            print("Well done, you guessed it")
+            print("Please guess higher")
         else: 
             print("Please guess lower")
     #     guess =int(input())
